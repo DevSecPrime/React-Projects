72cfa53010483db620eb3c00316c0817b81417e5
DevSecPrime <ranaaryan620@gmail.com>
2025-08-04 22:50:36 +0530

    feat: understand JSX

---
+++ 01.Basics/react-jsx-understanding/src/App.jsx
@@ -0,0 +1,9 @@
+import JSXRules from "./Components/JSXRules";
+const App = () => {
+  return (
+    <>
+      <JSXRules />
+    </>
+  );
+};
+export default App;
---
+++ 01.Basics/react-jsx-understanding/src/Components/JSXRules.jsx
@@ -0,0 +1,15 @@
+const JSXRules = () => {
+  return (
+    <>
+      <h1>JSX Rules</h1>
+      <p>JSX must return a single parent element.</p>
+      <p>JSX must be properly closed</p>
+      <p>
+        JSX attributes are written in camelCase (e.g. className instead of
+        class)
+      </p>
+    </>
+  );
+};
+
+export default JSXRules;
