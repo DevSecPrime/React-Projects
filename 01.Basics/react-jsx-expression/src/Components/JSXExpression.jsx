ea52eacaef24aba3130ce8e765b7e51569c880a7
DevSecPrime <ranaaryan620@gmail.com>
2025-08-05 23:01:56 +0530

    test: passing expression

--- 01.Basics/react-jsx-expression/src/Components/JSXExpression.jsx
+++ 01.Basics/react-jsx-expression/src/Components/JSXExpression.jsx
@@ -1,12 +1,14 @@
 const JSXExpression = () => {
   const multiply = (a, b) => a * b;
   const className = "jsx-expression";
+  const name = "Mr. Prime";
 
   return (
     <>
       <p>2 + 2 = {2 + 2}</p>
       <p>2 * 2 = {multiply(2, 2)}</p>
       <p className={className}>This is Special class</p>
+      <p>Hello!, {name}</p>
     </>
   );
 };
